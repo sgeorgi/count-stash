a35bc8572723a5c334fba421d029e8d5708f829f
Sebastian Georgi <sgeorgi@sgeorgi.de>
2014-08-22 16:27:34 +0200

    UnknownMessage and proper actor instantiation with companion object

--- src/main/scala/de/sgeorgi/countstash/Message.scala
+++ src/main/scala/de/sgeorgi/countstash/Message.scala
@@ -8,6 +8,8 @@ object Event {
 
   case class HandshakeRequest(shaker: String) extends Message
 
+  case class UnknownMessage(bs: ByteString)
+
   case object ConnectionClosed extends Message
 
 }
@@ -21,8 +23,9 @@ object Command {
 object MessageDecoder {
   def apply(bs: ByteString) = {
     /* Guard for CTRL-C do handle disconnecting from the testing console */
-    if (bs == ByteString(-1, -12, -1, -3, 6)) Command.TerminateHandler else {
-      bs
+    if (bs == ByteString(-1, -12, -1, -3, 6)) Command.TerminateHandler
+    else {
+      Event.UnknownMessage(bs)
     }
   }
 }
--- src/main/scala/de/sgeorgi/countstash/Server.scala
+++ src/main/scala/de/sgeorgi/countstash/Server.scala
@@ -2,10 +2,9 @@ package de.sgeorgi.countstash
 
 import java.net.InetSocketAddress
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.io.Tcp.{Bind, Received, Register}
-import akka.io._
-import akka.util.ByteString
+import akka.io.{IO, Tcp}
 
 class Server extends Actor with ActorLogging {
 
@@ -14,34 +13,33 @@ class Server extends Actor with ActorLogging {
   IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 8081))
 
   def receive = {
-
     case Tcp.Connected(remote, local) =>
-      val serverWorker = context.actorOf(Props[ConnectionHandler])
+      val serverWorker = context.actorOf(ConnectionHandler.props(self))
       sender ! Register(serverWorker)
       log info s"Client from ${remote.getHostString} connected"
-
     case Tcp.Bound(localAddress) =>
       log info s"Server bound to local address $localAddress"
-
     case Tcp.CommandFailed(cmd) =>
       log warning s"TCP Command failed: $cmd"
+
   }
 
 }
 
-class ConnectionHandler extends Actor with ActorLogging {
+object ConnectionHandler {
+  def props(server: ActorRef): Props = Props(new ConnectionHandler(server))
+}
+
+class ConnectionHandler(server: ActorRef) extends Actor with ActorLogging {
   def receive = {
-
     case Received(data) =>
-      /* Handled off to MessageDecoder and send the resulting Message to ourself */
+      /* Handle off to MessageDecoder and send the resulting Message to ourself */
       self ! MessageDecoder(data)
-
     case Command.TerminateHandler =>
       log info s"Client want's to disconnect, stopping ConnectionHandler"
       context stop self
-
-    case data: ByteString =>
-      log info s"Received data '$data'"
+    case Event.UnknownMessage(data) =>
+      log info s"Received UnknownMessage with '$data'"
 
   }
 }
